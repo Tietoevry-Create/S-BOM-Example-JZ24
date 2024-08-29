f9117e9ee3f28bb56b835b2e03863232e0324a5c
Kent Inge Fagerland Simonsen <40064+kentis@users.noreply.github.com>
2024-08-29 10:06:11 +0200

    Initial java project (#1)
    * initial version (WIP)
    
    * Use cowsay in output
    
    * Update pom.xml
    
    Remove sbom generation in build

---
+++ sbom-demo-java/src/main/java/no/javazone/App.java
@@ -0,0 +1,9 @@
+package no.javazone;
+import com.github.ricksbrown.cowsay.*;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( Cowsay.say( new String[] {"Hello Javazone!"}) );
+    }
+}
