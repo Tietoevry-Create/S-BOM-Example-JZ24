ca38da4ae16205f7b63579768886cae954c0dc86
Kent Inge Fagerland Simonsen <40064+kentis@users.noreply.github.com>
2024-08-29 09:54:14 +0200

    Use cowsay in output

--- sbom-demo-java/src/main/java/no/javazone/App.java
+++ sbom-demo-java/src/main/java/no/javazone/App.java
@@ -1,9 +1,9 @@
 package no.javazone;
-
+import com.github.ricksbrown.cowsay.*;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Javazone!" );
+        System.out.println( Cowsay.say( new String[] {"Hello Javazone!"}) );
     }
 }
